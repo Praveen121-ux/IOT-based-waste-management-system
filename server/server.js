ce2ec8cd01841c70a46d0602d8c662e7b3ff877c
Praveen121-ux <praveenk121004@gmail.com>
2026-04-03 13:56:54 +0530

    Update server.js
    Replace service account key loading method with environment variable.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const cors = require('cors');
 const axios = require('axios');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
